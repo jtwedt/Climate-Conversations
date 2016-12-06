0294ed33b03bb5d8ae8bd8c07fbe824ac4e5e6a7
Rachel White <rhwhite32@gmail.com>
2016-12-06 12:21:28 -0800

    read in excel file and make delay between questions

--- ClimateConversations.py
+++ ClimateConversations.py
@@ -1,7 +1,15 @@
 import pdb
+import pandas as pd
+import time
+
 # This is the stub program
 
 # Load the events database RACHEL
+
+xlsx = pd.ExcelFile('firstHistoricClimateEvents.xlsx')
+df = xlsx.parse(xlsx.sheet_names[0])
+events = df.to_dict()
+#print events.keys()    # We may want to change some of the key names to be more efficient for indexing!
 
 # Get input from user (name, birthdate, number of rounds) JUDY
 print('Welcome! Lets get a little information before we start the conversation.')
@@ -9,6 +17,9 @@ nplayers  = input('How many people are in the conversation? ')
 nrounds = input('How many rounds would you like to play? ')
 names = []
 birthyears = []
+
+# Work out how long to delay between questions
+delayinseconds = 5
 
 indices = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th']
 for i in range(nplayers):
@@ -18,7 +29,14 @@ for i in range(nplayers):
     birthyears.append(thisage)
 
 # Loop over each user
-for j in nrounds:
-    for k in nplayers:
+next = []
+for j in range(0,nrounds):
+    for k in range(0,nplayers):
         # Determine the filter and selection of events
         # display event, ask questions
+        print('here is your question')
+
+        # Have count-down time before prompting
+        time.sleep(delayinseconds)
+        raw_input("You've been talking about this question for " + str(delayinseconds / 60) + " minutes, press enter when you are ready to move on to the next question?")
+
