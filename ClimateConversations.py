f170856322f93972099ac38aad15a528dcd03985
Rachel White <rhwhite32@gmail.com>
2016-12-08 17:53:29 -0800

    correcting and adding error handling of player input data

--- ClimateConversations.py
+++ ClimateConversations.py
@@ -17,7 +17,7 @@ nevents = len(events['start year'])
 
 delayinseconds = 1
 
-# set up empty lists
+# set up lists and global variables
 names = []
 birthyears = []
 
@@ -29,45 +29,70 @@ def is_number(s):
     except ValueError:
         return False
 
-# Get input from user (name, birthdate, number of rounds) JUDY
+def getplayerinfo():
+    # Get input from user (name, birthdate, number of rounds) JUDY
+    if test == 1:
+        nplayers = 2
+        nrounds = 2
+        names.append('Judy')
+        names.append('Rachel')
+        birthyears.append(1982)
+        birthyears.append(1986)
+
+    else:
+        nplayers  = input('How many people are in the conversation? ')
+        nrounds = input('How many rounds would you like to play? ')
+
+        # Work out how long to delay between questions
+        delayinseconds = 1
+
+        indices = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th']
+        for i in range(nplayers):
+            thisname = raw_input('Please enter the ' + indices[i] + ' players name: ')
+            names.append(thisname)
+            
+
+            needage = True
+            while needage:
+                thisage = raw_input('Please enter ' + thisname + '\'s year of birth: ')
+
+                if is_number(thisage):
+                    while (int(thisage) < 1880 or int(thisage) > datetime.datetime.now().year):
+                        thisage = input('You are either over 130 years old, or haven\'t been born yet. Please re-enter ' + thisname + '\'s players year of birth: ')
+                    birthyears.append(int(thisage))
+                    needage = False
+                else:
+                    print('Please enter a number, for example, 1980')   
+                
+    return(nplayers,nrounds,names,birthyears)   # return these variables so they are globally known
+
+# Main program
 print('Welcome! Lets get a little information before we start the conversation.')
-
 test  = input('Is this a test run? 1= yes, 0 = no ')
-if test == 1:
-    nplayers = 2
-    nrounds = 2
-    names.append('Judy')
-    names.append('Rachel')
-    birthyears.append(1982)
-    birthyears.append(1986)
 
-else:
-    nplayers  = input('How many people are in the conversation? ')
-    nrounds = input('How many rounds would you like to play? ')
-
-    # Work out how long to delay between questions
-    delayinseconds = 1
-
-    indices = ['1st', '2nd', '3rd', '4th', '5th', '6th', '7th', '8th', '9th', '10th']
+needinfo = True
+while needinfo:
+    nplayers,nrounds,names,birthyears = getplayerinfo()
+    # Print out a summary of players and dates of birth
+    os.system('clear')
+    print('These are the players for this game:')
     for i in range(nplayers):
-        thisname = raw_input('Please enter the ' + indices[i] + ' players name: ')
-        thisage = input('Please enter ' + thisname + '\'s year of birth: ')
-        if is_number(thisage):
-            while (int(thisage) < 1880 or int(thisage) > datetime.datetime.now().year):
-                thisage = input('You are either over 130 years old, or haven\'t been born yet. Please re-enter ' + thisname + '\'s players year of birth: ')
-            birthyears.append(int(thisage))
- 
-        names.append(thisname)
-        birthyears.append(thisage)
+        print(names[i] + ', born in ' + str(birthyears[i]))
+    print(' ')
+    print('You want to play ' + str(nrounds) + ' rounds')
+    print(' ')
+    correctinput = raw_input('Is this correct? Yes/No? ')
 
-# Print out a summary of players and dates of birth
-print('These are the players for this game:')
-for i in range(nplayers):
-    print(names[i] + ', born in ' str(birthyears[i]))
-print(' ')
-correctinput = input('Is this correct? Yes/No? ')
-
-#if correctinput ==
+    while correctinput not in ['Yes','yes']:
+        if correctinput in ['No','no']:
+            names = []
+            birthyears = [] 
+            print('Ok, let\'s try again')
+            break
+        else:
+            correctinput = raw_input('I\'m sorry, I didn\'t understand that. Is the game information correct, please enter Yes or No ')
+    else:
+        needinfo=False
 
 
 # Each user gets a set of indices for the events
