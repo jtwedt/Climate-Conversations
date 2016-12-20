b706c27d796db5c861d29d8b9f90f7fc13c4ce08
Rachel White <rhwhite32@gmail.com>
2016-12-20 12:42:38 -0800

    adding test Kivy program

---
+++ ccKivy.py
@@ -0,0 +1,21 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty, ReferenceListProperty,\
+    ObjectProperty
+from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
+from kivy.graphics import Color, Rectangle
+from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+
+class ccGame(Widget):
+    pass
+class ccApp(App):
+    def build(self):
+        return ccGame()
+
+
+if __name__ == '__main__':
+    ccApp().run()
+
