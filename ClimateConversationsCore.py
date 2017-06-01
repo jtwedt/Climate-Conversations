bd4524779c1e95ade64243ddb082c2d990a70856
Katie <k.doroschak@gmail.com>
2017-05-31 20:39:40 -0700

    added auto documentation

--- ClimateConversationsCore.py
+++ ClimateConversationsCore.py
@@ -39,28 +39,36 @@ class Conversation():
         self.gen_questions = list(pd.read_table(general_q_file, comment="#", header=None)[0])
         self.q_colnames = self.find_question_cols()
 
-    '''
-    Load event spreadsheet from an excel file. 
-    #TODO write format this should be in here
-    Input: filename of excel spreadsheet
-    Output: dictionary of events (keys: event id, values: event information)
-    '''
+    
     def load_events_from_excel(self, events_file):
+        '''
+        Load event spreadsheet from an excel file.
+
+        #TODO write format this should be in here
+
+        **Input**: filename of excel spreadsheet
+
+        **Output**: dictionary of events (keys: event id, values: event information)
+        '''
         xlsx = pd.ExcelFile(events_file)
         df = xlsx.parse(xlsx.sheet_names[0])
         events = df.to_dict()
         return events
 
-    '''
-    Load event spreadsheet directly from google drive.
-    Input: gdrive key
-    Output: dictionary of events (keys: event id, values: event information)
-      The gdrive_key is the code in the URL after ../spreadsheets/d/
-      Example: https://docs.google.com/spreadsheets/d/1sjO-EcVfFZR8aJIT7br3UxYSmsVpoPAjPzmdNHToaXg/edit#gid=630120060
-      The key for the above url would be "1sjO-EcVfFZR8aJIT7br3UxYSmsVpoPAjPzmdNHToaXg"
-      You can get this via the "Share" button -> "get shareable link"
-    '''
+    
     def load_events_from_gdrive(self, gdrive_key):
+        '''
+        Load event spreadsheet directly from google drive.
+
+        **Input**: gdrive key
+
+        **Output**: dictionary of events (keys: event id, values: event information)
+
+          The `gdrive_key` is the code in the URL after `../spreadsheets/d/`
+          Example: `https://docs.google.com/spreadsheets/d/1sjO-EcVfFZR8aJIT7br3UxYSmsVpoPAjPzmdNHToaXg/edit#gid=630120060`
+          The key for the above url would be "`1sjO-EcVfFZR8aJIT7br3UxYSmsVpoPAjPzmdNHToaXg`"
+          You can get this via the "Share" button -> "get shareable link"
+        '''
         gdrive_url = "https://docs.google.com/spreadsheet/ccc?key=" + gdrive_key + "&output=csv"
         df = pd.read_csv(gdrive_url, encoding='utf-8', skiprows=1) # header keeps changing
         events = df.to_dict()
---
+++ docs/README.md
@@ -0,0 +1,9 @@
+# Auto-generate documentation
+
+Run this from inside the docs directory:
+
+    ./make-html
+
+Then run this to open the docs in your default browser:
+
+    open play_webapp.m.html; open ClimateConversationsCore.m.html
